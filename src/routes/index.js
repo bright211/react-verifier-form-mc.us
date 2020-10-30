f0c42154b88dc92628515f11f63cdc4f2751fd4d
bright211 <fomafomin721@gmail.com>
2020-10-30 14:05:20 +1000

    terms page

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,43 +5,21 @@ import QuoteDetailSecond from "../pages/quotedetailsecond/quotedetailsecond";
 import ProcessBar from "../pages/processbar/processbar";
 import HomePage from "../pages/homepage.js";
 import Login from "../pages/login";
-import PrivacyPage from '../pages/privacypage';
+import PrivacyPage from "../pages/privacypage";
+import TermsPage from "../pages/TermsPage";
 
 function Routes() {
   return (
-      <Switch>
-        <Route
-          exact
-          path={`/quotedetailfirst`}
-          component={QuoteDetailFirst}
-          />
-        <Route
-          exact
-          path={`/quotedetailsecond`}
-          component={QuoteDetailSecond}
-        />
-        <Route
-          exact
-          path={`/processbar`}
-          component={ProcessBar}
-        />
-        <Route
-          exact
-          path={`/homepage`}
-          component={HomePage}
-        />    
-        <Route
-          exact
-          path={`/login`}
-          component={Login}
-        />    
-        <Route
-          exact
-          path={`/privacy`}
-          component={PrivacyPage}
-        />
-        <Redirect from="/" to='quotedetailfirst' />
-      </Switch>
+    <Switch>
+      <Route exact path={`/quotedetailfirst`} component={QuoteDetailFirst} />
+      <Route exact path={`/quotedetailsecond`} component={QuoteDetailSecond} />
+      <Route exact path={`/processbar`} component={ProcessBar} />
+      <Route exact path={`/homepage`} component={HomePage} />
+      <Route exact path={`/login`} component={Login} />
+      <Route exact path={`/privacy`} component={PrivacyPage} />
+      <Route exact path={`/termsandcondition`} component={TermsPage} />
+      <Redirect from="/" to="quotedetailfirst" />
+    </Switch>
   );
 }
 
